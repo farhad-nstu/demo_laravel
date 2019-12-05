037ddabc6b55f093dbee1826aebc58b7a4842cc6
farhad-nstu <amfarhad33@gmail.com>
2019-12-05 18:58:12 +0600

    first commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    public function index(){
+    	return Product::orderBy('created_at', 'desc')->get();
+    }
+
+    public function store(Request $request){
+    	$product = Product::create($request->all());
+        return $product;
+    }    	
+}
